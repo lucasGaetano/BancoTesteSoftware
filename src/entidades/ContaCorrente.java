c8cf4eb020915cb8e911785aa37c9560b00ac4a9
Lucas Augusto <luccasgaetano19@gmail.com>
2016-08-11 01:58:31 -0300

    implementação dos métodos saldo, sacar e depositar da ContaCorrente

--- src/entidades/ContaCorrente.java
+++ src/entidades/ContaCorrente.java
@@ -1,22 +1,25 @@
 package entidades;
 
 public class ContaCorrente implements Conta {
+	private double saldo;
 
+	public ContaCorrente(double saldo) {
+		this.saldo = saldo;
+	}
+	
 	@Override
 	public double saldo() {
-		// TODO Auto-generated method stub
-		return 0;
+		return saldo;
 	}
 
 	@Override
 	public void sacar(double valor) {
-		// TODO Auto-generated method stub
-		
+		this.saldo = saldo - valor;
 	}
 
 	@Override
 	public void depositar(double valor) {
-		// TODO Auto-generated method stub
+		this.saldo = saldo + valor;
 		
 	}
 

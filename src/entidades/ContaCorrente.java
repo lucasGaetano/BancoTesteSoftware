2b80968bb8e3149da4003010d120be135fd56e36
Lucas Augusto <luccasgaetano19@gmail.com>
2016-08-10 21:15:33 -0300

    adicionado classes ContaCorrente e Conta Poupanca

---
+++ src/entidades/ContaCorrente.java
@@ -0,0 +1,23 @@
+package entidades;
+
+public class ContaCorrente implements Conta {
+
+	@Override
+	public double saldo() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void sacar(double valor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void depositar(double valor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

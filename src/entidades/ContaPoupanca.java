510f8e6b85f08b9cf653cea86f261a4b36ed5728
Lucas Augusto <luccasgaetano19@gmail.com>
2016-08-11 18:14:56 -0300

    adicionado regras de negocio a conta poupanca; Adicionado limite de credito para contaPoupanca

--- src/entidades/ContaPoupanca.java
+++ src/entidades/ContaPoupanca.java
@@ -1,23 +1,39 @@
 package entidades;
 
 public class ContaPoupanca implements Conta {
-
+	private double saldo;
+	private double limite;
+	
+	public ContaPoupanca(double limite) {
+		this.limite = limite;
+	}
+	
 	@Override
 	public double saldo() {
-		// TODO Auto-generated method stub
-		return 0;
+		return saldo;
+	}
+	public double getLimite() {
+		return this.limite;
 	}
 
 	@Override
 	public void sacar(double valor) {
-		// TODO Auto-generated method stub
-		
+		if(valor < 0)
+			throw new IllegalArgumentException("Não é premitido sacar valores menores que zero");
+		if(saldo <= 0)
+			throw new IllegalArgumentException("Não tem saldo na conta");
+		if(saldo - valor < 0)
+			throw new IllegalArgumentException("Operação não permitida!");
+		this.saldo = saldo - valor;
 	}
 
 	@Override
 	public void depositar(double valor) {
-		// TODO Auto-generated method stub
-		
+		if(valor < 0)
+			throw new IllegalArgumentException("Não é premitido depositar valores menores que zero");
+		if(valor > 3000)
+			throw new IllegalArgumentException("O limite do depósito é R$ 3.000,00");
+		this.saldo = saldo + valor;
 	}
 
 }

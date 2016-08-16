5c131eb7b333c9426ebf202776225a082ac06b88
Lucas Augusto <luccasgaetano19@gmail.com>
2016-08-15 22:58:29 -0300

    corrigido design da classe transferencia

--- src/entidades/Transferencia.java
+++ src/entidades/Transferencia.java
@@ -1,5 +1,19 @@
 package entidades;
 
-public class Transferencia {
+public class Transferencia implements Transacao {
+	private Conta de;
+	private Conta para;
+	private double valor;
+	
+	public Transferencia(Conta de, Conta para, double valor) {
+		this.de = de;
+		this.para = para;
+		this.valor = valor;
+	}
 
+	@Override
+	public void executa() {
+		de.sacar(valor);
+		para.depositar(valor);
+	}
 }
